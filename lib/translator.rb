b7c40d96603011ddd4ca6de9742945d4d8cc8f4d
Mar Mustafa <amustaf@email.arizona.edu>
2020-07-27 16:44:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -37,7 +37,7 @@ def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
     result[key][:japanese] == japanese_emoticon
   end
   if return_value
-    result[return_value]
+    result[return_value][:english]
   else
     "Sorry, that emoticon was not found"
   end
